9d89d65b7491a3f1e4781bece2c46161e9173112
Andrew <andrewaacodo12@gmail.com>
2020-05-10 18:55:29 +0200

    Splash Screen

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 6;
+    int currentSceneIndex;
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //Splash Screen Scene we need to wait for seconds
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+
+    // A Courotine that wait for seconds and then return
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
