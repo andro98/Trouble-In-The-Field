2fc9d3a8dedaf4cc70215d934baef6a9c5f0c71e
Andrew <andrewaacodo12@gmail.com>
2020-05-13 14:30:59 +0200

    Adding Event keyfram in Animator

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -3,10 +3,14 @@
 // This class For enemies attackers to move to left by each update
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)]
-    [SerializeField] float moveSpeed = 1.0f;
+    float currentSpeed = 0.0f;
     void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+        transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
+    }
+    // THIS FUNCION WILL BE FIRED FROM THE ANIMATION TIMELINE
+    public void SetMovementSpeed(float speed)
+    {
+        this.currentSpeed = speed;
     }
 }
