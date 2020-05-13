eafc4136fed15829d265407136e6cbaa8779302e
Andrew <andrewaacodo12@gmail.com>
2020-05-13 23:48:04 +0200

    Health and Damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    public void DealDamage(float damage)
+    {
+        health -= damage;
+        Debug.Log(health);
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,12 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1.0f;
+    [SerializeField] float damage = 100.0f;
     void Update()
     {
         transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+    }
+    //  IF PROJECTILE TRIGGER WITH AN ATTACKER THAT HAS A HEALTH SCRIPT
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var health = other.GetComponent<Health>();
+        health.DealDamage(damage);
     }
 }
