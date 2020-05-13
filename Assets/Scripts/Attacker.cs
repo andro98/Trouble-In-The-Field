587d94ad88829d3125db7c64790b42f6286c4549
Andrew <andrewaacodo12@gmail.com>
2020-05-13 13:25:48 +0200

    Translate Enemie WIth Attacker Script

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+// This class For enemies attackers to move to left by each update
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    [SerializeField] float moveSpeed = 1.0f;
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+    }
+}
