32998ee31cb1be1a044a8fd8685f694d6118283a
Andrew <andrewaacodo12@gmail.com>
2020-05-13 13:40:39 +0200

    Spawning Attackers

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+// THIS CLASS START TO SPAWN ENEMIES ATTACKER 
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] Attacker attackerPrefab;
+    [SerializeField] float minSpawnDelay = 1.0f;
+    [SerializeField] float maxSpawnDelay = 5.0f;
+    bool spawn = true; //To control spawning at the start and the end of the level
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+}
