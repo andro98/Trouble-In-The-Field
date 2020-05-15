5ef83438499e491ff501066edc02d313c9349048
Andrew <andrewaacodo12@gmail.com>
2020-05-15 16:20:53 +0200

    Spawning defender and Snap it to the Grid

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+    private void OnMouseDown()
+    {
+        SpawnDefender(GetSquareClicker());
+    }
+    private Vector2 GetSquareClicker()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);     // To Convert the position to the grid system we have
+        Vector2 gridPos = SnapToGrid(worldPos);
+        return gridPos;
+    }
+    private Vector2 SnapToGrid(Vector2 pos)
+    {
+        float newX = Mathf.RoundToInt(pos.x);
+        float newY = Mathf.RoundToInt(pos.y);
+        return new Vector2(newX, newY);
+    }
+    private void SpawnDefender(Vector2 pos)
+    {
+        GameObject newDefender = Instantiate(defender, pos, Quaternion.identity) as GameObject;
+    }
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
     [SerializeField] float health = 100f;
     public void DealDamage(float damage)
     {
@@ -11,7 +12,14 @@ public class Health : MonoBehaviour
         Debug.Log(health);
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(this.gameObject);
         }
+    }
+    private void TriggerDeathVFX()
+    {
+        if (!deathVFX) { return; }
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXObject, 0.5f);
     }
 }
