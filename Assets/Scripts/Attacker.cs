c2cb34677537a5bf1da3052897570b31669c145b
Andrew <andrewaacodo12@gmail.com>
2020-05-23 17:23:51 +0200

    Updating Attacker Animation

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 // This class For enemies attackers to move to left by each update
 public class Attacker : MonoBehaviour
@@ -8,7 +9,16 @@ public class Attacker : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * Time.deltaTime * currentSpeed);
+        UpdateAnimationState();
     }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+            GetComponent<Animator>().SetBool("isAttacking", false);
+
+    }
+
     // THIS FUNCION WILL BE FIRED FROM THE ANIMATION TIMELINE
     public void SetMovementSpeed(float speed)
     {
@@ -19,5 +29,13 @@ public class Attacker : MonoBehaviour
     {
         GetComponent<Animator>().SetBool("isAttacking", true);
         currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) return;
+
+        Health health = currentTarget.GetComponent<Health>();
+        if (health) { health.DealDamage(damage); }
     }
 }
