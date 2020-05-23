286e44e42fcd7a4cf5e1252c7a13b36a83b02dd0
Andrew <andrewaacodo12@gmail.com>
2020-05-23 13:04:55 +0200

    Attack only if enemy on lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,6 +4,37 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] Transform gunPosition;
     [SerializeField] GameObject projectile;
+    Animator animator;
+    AttackerSpawner myLaneSpawner;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        bool attack = IsAttackerInLane();
+        animator.SetBool("IsAttacking", attack);
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spwaner in attackerSpawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spwaner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spwaner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        return myLaneSpawner.transform.childCount > 0;
+    }
+
     public void Fire()
     {
         Instantiate(projectile, gunPosition.position, transform.rotation);
